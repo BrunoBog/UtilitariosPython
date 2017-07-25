3705ead4b945439523f8ef90df2e42c48c82003a
BOG <bog906@gmail.com>
2017-07-25 09:13:06 -0300

    refract

--- HardMobHunter/src/app.py
+++ HardMobHunter/src/app.py
@@ -1,47 +1,41 @@
+from datetime import time
+from time import *
 import requests
 import time
-from time import gmtime, strftime
 from bs4 import BeautifulSoup
-from win10toast import ToastNotifier
+from flask import Flask
+from src.Busca import hunting
 
-print("Procurando Itens no HardMob...")
-# caminhoIMG = "C:/Users/BOG/PycharmProjects/HardMobHunter/img/icons8-Bilhete Desconto-64.ico"
-while True:
-    request = requests.get("http://www.hardmob.com.br/promocoes/")
-    content = request.content
-    soup = BeautifulSoup(content, "html.parser")
-    element = soup.find_all("a", {"class": "title"})
-    toaster = ToastNotifier()
-    for item in element:
-        if "JBL" in item.text or "Monitor" in item.text or "Cadeira" in item.text:
-            print("Encontrei o tópico : " + (item.text.replace("\t", "")).replace("\n", ""))
-            print(item.attrs['href'])
-            toaster.show_toast("Achei!", item.text, duration=10)
+app = Flask(__name__)
 
-    print("Busca no promobit")
-    request = requests.get("https://www.promobit.com.br/")
-    content = request.content
-    soup = BeautifulSoup(content, "html.parser")
-    element = soup.find_all("a", {"itemprop": "name", "class": "access_url "})
-    toaster = ToastNotifier()
-    for item in element:
-        if "JBL" in item.text or "Monitor" in item.text or "Cadeira" in item.text:
-            print("Encontrei o tópico : " + (item.text.replace("\t", "")).replace("\n", ""))
-            print("https://www.promobit.com.br/" + item.attrs['href'])
-            toaster.show_toast("Achei!", item.text, duration=10)
+@app.route('/')
+def main():
+    return hunt()
 
-    print("Busca no Pelando")
-    request = requests.get("https://www.pelando.com.br/")
-    content = request.content
-    soup = BeautifulSoup(content, "html.parser")
-    element = soup.find_all("a", {"itemprop": "name", "class": "cept-tt linkPlain space--r-1 space--v-1 "})
-    toaster = ToastNotifier()
-    for item in element:
-        if "JBL" in item.text or "Monitor" in item.text or "Cadeira" in item.text:
-            print("Encontrei o tópico : " + (item.text.replace("\t", "")).replace("\n", ""))
-            print("https://www.pelando.com.br/" + item.attrs['href'])
-            toaster.show_toast("Achei!", item.text, duration=10)
-            #toaster.show_toast("Achei!", item.text, icon_path=caminhoIMG, duration=10)
 
-    print("Executado em:" + strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-    time.sleep(60*2)
+def hunt():
+    while True:
+        print("Iniciando as buscas... " + strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+        print("Busca no HardMob...")
+        request = requests.get("http://www.hardmob.com.br/promocoes/")
+        content = request.content
+        soup = BeautifulSoup(content, "html.parser")
+        element = soup.find_all("a", {"class": "title"})
+        hunting(element, "Monitor")
+        hunting(element, "Z2")
+
+        print("Busca no promobit")
+        request = requests.get("https://www.promobit.com.br/")
+        content = request.content
+        soup = BeautifulSoup(content, "html.parser")
+        element = soup.find_all("a", {"itemprop": "name", "class": "access_url "})
+        hunting(element, "Monitor")
+        hunting(element, "Z2")
+
+        print("Executado em:" + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + "\n")
+        time.sleep(60*5)
+
+if __name__ == '__main__':
+    hunt()
+
+
